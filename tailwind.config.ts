1d6bcc30c46596caa9d61509da1aff9f294b6d1c
PavelM <myagkiypavel@gmail.com>
2024-12-06 17:44:31 +0200

    feat: add clsx svgr prettier-tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import { fontFamily } from "tailwindcss/defaultTheme";
 
 export default {
   content: [
@@ -8,9 +9,23 @@ export default {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        raleway: ["var(--font-raleway)", ...fontFamily.sans],
+        bitter: ["var(--font-bitter)", ...fontFamily.sans],
+      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        gray_light: "#E0E0E0",
+        gray_medium: "#C2C2C2",
+        gray_dark: "#536D7A",
+        blue_dark: "#1D2F38",
+        blue_light: "#AABCC5",
+        blue_gray: "#768F9C",
+        green: "#17A550",
+        yellow: "#FFC728",
+        pink_light: "#FFB6B6",
+        pink: "#FFB8B8",
       },
     },
   },
