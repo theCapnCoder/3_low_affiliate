bed90bb3061c6a30b950f1bc5357f19fca157631
PavelM <myagkiypavel@gmail.com>
2024-12-07 19:31:09 +0200

    feat: NewsLetter markUp

--- src/app/_containers/newsletter.tsx
+++ src/app/_containers/newsletter.tsx
@@ -1,29 +1,38 @@
 import Image from "next/image";
 import Mail from "../../../public/img/mail.jpg";
 import { MailLarge } from "@/assets/svg";
+import { Button } from "../_components/button";
 
 export const Newsletter = () => {
   return (
-    <section>
-      <div className="flex gap-4">
+    <section className="py-20">
+      <div className="flex gap-14 bg-[url('/img/pattern_large.png')] p-12">
         <Image src={Mail} alt="mail" />
 
         <div>
-          <div className="flex items-center gap-4">
-            <MailLarge />
+          <div className="mb-8 flex items-center gap-3 pt-3">
+            <div className="flex h-14 w-14 items-center justify-center rounded-full bg-white">
+              <MailLarge />
+            </div>
             <h3 className="text-3xl">Newsletter Signup</h3>
           </div>
 
-          <p className="text-lg leading-snug">
+          <p className="mb-5 text-lg leading-snug">
             Be the first to receive updates on articles, product reviews,
             courses, and more!
           </p>
 
-          <input type="text" placeholder="Your email" />
+          <input
+            type="text"
+            placeholder="Your email"
+            className="mb-7 w-full p-4 placeholder:text-xs placeholder:text-blue_gray"
+          />
 
-          <div className="flex items-center gap-4">
-            <button>Submit</button>
-            <p className="text-sx">Privacy Policy</p>
+          <div className="flex items-center gap-2">
+            <Button yellow size="md">
+              Submit
+            </Button>
+            <p className="text-green_dark text-xs">Privacy Policy</p>
           </div>
         </div>
       </div>
