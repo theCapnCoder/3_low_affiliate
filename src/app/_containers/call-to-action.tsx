ac0bee0053f9622902568cf91733fe346ab30ef2
PavelM <myagkiypavel@gmail.com>
2024-12-07 15:08:45 +0200

    feat: CallToAction markUp

--- src/app/_containers/call-to-action.tsx
+++ src/app/_containers/call-to-action.tsx
@@ -1,17 +1,22 @@
 import { Tools } from "@/assets/svg";
+import { Button } from "../_components/button";
 
 export const CallToAction = () => {
   return (
-    <section>
-      <div className="flex items-center justify-between gap-4">
-        <div className="flex gap-4 items-center">
+    <section className="py-20">
+      <div className="flex items-center justify-between gap-4 bg-[url('/img/pattern_small.png')] p-7">
+        <div className="flex items-center gap-3">
           <Tools />
-          <h3 className="font-bitter text-3xl">
+          <h3 className="font-bitter text-3xl tracking-tight">
             View our list of the best products in lawncare.
           </h3>
         </div>
 
-        <div>See the list</div>
+        <div className="pr-3">
+          <Button size="sm" green>
+            See the list
+          </Button>
+        </div>
       </div>
     </section>
   );
