0150c8bcca1212e3f49b1caab659bcf0e063bda5
PavelM <myagkiypavel@gmail.com>
2024-12-07 14:57:13 +0200

    feat: latest markUp

--- src/app/_containers/latest.tsx
+++ src/app/_containers/latest.tsx
@@ -5,33 +5,35 @@ import { LatestCards } from "./data";
 
 export const Latest = () => {
   return (
-    <section className="text-blue_dark">
-      <h3 className="font-bitter text-4xl">Latest from the Blog</h3>
+    <section className="py-5 text-blue_dark">
+      <h3 className="mb-8 font-bitter text-4xl tracking-wide">
+        Latest from the Blog
+      </h3>
 
-      <div className="grid grid-cols-2">
-        <div className="col-span-3 row-span-1">
-          <Image src={SlideLarge} alt="slide_large" />
+      <div className="grid grid-cols-2 grid-rows-3 gap-x-10 gap-y-5 mb-20">
+        <div className="col-span-1 row-span-3">
+          <div className="relative mb-2 max-h-[400px] h-full">
+            <Image src={SlideLarge} alt="slide_large" fill objectFit="cover" />
+          </div>
 
-          <h4 className="font-bitter text-3xl">
+          <h4 className="mb-3 font-bitter text-3xl">
             How to Prep Your Lawn in the Spring
           </h4>
-          <p className="leading-tight">
+          <p className="tracking-tighte max-w-[53ch] leading-tight">
             This is the blog description text. This is the blog description
             text. This is the blog description text.
           </p>
         </div>
 
-        <div>
-          {LatestCards.map((item, index) => (
-            <div key={index} className="flex items-center gap-4">
-              <Image src={item.img} alt={item.alt} />
-              <p className="text-lg">{item.text}</p>
-            </div>
-          ))}
-        </div>
+        {LatestCards.map((item, index) => (
+          <div key={index} className="flex items-center gap-4">
+            <Image src={item.img} alt={item.alt} />
+            <p className="text-lg">{item.text}</p>
+          </div>
+        ))}
       </div>
 
-      <div className="flex gap-4">
+      <div className="flex gap-1">
         <Play />
         <p>View All Blog Articles</p>
       </div>
